769e30413d099d555f563162f9d61f43fa610686
commitnotliving <hmigner@gmail.com>
2019-01-28 09:24:45 -0500

    Création des variables

--- src/CV.java
+++ src/CV.java
@@ -1,9 +1,25 @@
 public class CV {
 	
+	public String nom,
+	  			  prenom,
+	  			  formation,
+	  			  competences,
+	  			  attentes;
+	public int experiences;
 
+	public CV(String pnom, String pprenom, String pformation, int pexperiences, String pcompetences, 
+		String pattentes){
+		nom = pnom;
+		prenom = pprenom;
+		formation = pformation;
+		experiences = pexperiences;
+		competences = pcompetences;
+		attentes = pattentes;
+}
 
 	public static void main( String[] args ) {
 
+		
 	}
 	
 }
